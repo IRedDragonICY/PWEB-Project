85946c10711294300d28307919dd759298ce568b
IRedDragonICY <hendik.suwoto@gmail.com>
2023-05-31 14:19:20 +0700

    change variable to english

--- js/createAccount.js
+++ js/createAccount.js
@@ -1,7 +1,7 @@
 document.getElementById('register-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const nama = document.getElementById('nama').value;
+    const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
@@ -10,4 +10,29 @@ document.getElementById('register-form').addEventListener('submit', function (ev
         alert('Password dan konfirmasi password tidak cocok.');
         return;
     }
+    // input data to Azure SQL
+    fetch('https://api-azure-sql.herokuapp.com/api/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },  
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+        }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+            alert('Pendaftaran berhasil!');
+            window.location.href = 'login.html';
+        }
+        )
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('Pendaftaran gagal!');
+        }
+        );
+
 });
