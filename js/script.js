be8328dd03e7432feee5ffdb5200539fc58ecd61
EvindaAprl <91203052+EvindaAprl@users.noreply.github.com>
2023-06-29 00:35:56 +0700

    fix submenuItems

--- js/script.js
+++ js/script.js
@@ -5,23 +5,29 @@ const submenuItems = document.querySelectorAll(".submenu_item");
 const sidebarOpen = document.querySelector("#sidebarOpen");
 const sidebarClose = document.querySelector(".collapse_sidebar");
 const sidebarExpand = document.querySelector(".expand_sidebar");
+
 sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+
 sidebarClose.addEventListener("click", () => {
   sidebar.classList.add("close", "hoverable");
 });
+
 sidebarExpand.addEventListener("click", () => {
   sidebar.classList.remove("close", "hoverable");
 });
+
 sidebar.addEventListener("mouseenter", () => {
   if (sidebar.classList.contains("hoverable")) {
     sidebar.classList.remove("close");
   }
 });
+
 sidebar.addEventListener("mouseleave", () => {
   if (sidebar.classList.contains("hoverable")) {
     sidebar.classList.add("close");
   }
 });
+
 darkLight.addEventListener("click", () => {
   body.classList.toggle("dark");
   if (body.classList.contains("dark")) {
@@ -31,16 +37,22 @@ darkLight.addEventListener("click", () => {
     darkLight.classList.replace("bx-moon", "bx-sun");
   }
 });
+
 submenuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
-    item.classList.toggle("show_submenu");
+    const menu = item.getAttribute("data-menu");
+    const submenu = document.querySelector(`.${menu}_submenu`); // Menemukan submenu yang sesuai berdasarkan atribut data-menu
+    submenu.classList.toggle("show_submenu");
     submenuItems.forEach((item2, index2) => {
       if (index !== index2) {
-        item2.classList.remove("show_submenu");
+        const otherMenu = item2.getAttribute("data-menu");
+        const otherSubmenu = document.querySelector(`.${otherMenu}_submenu`); // Menemukan submenu yang tidak sesuai dan menyembunyikannya
+        otherSubmenu.classList.remove("show_submenu");
       }
     });
   });
 });
+
 if (window.innerWidth < 768) {
   sidebar.classList.add("close");
 } else {
