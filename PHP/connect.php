de6bdece6ec1d068b4e3db6358a7c63b55916deb
Ndik <hendik.suwoto@gmail.com>
2023-07-02 22:44:07 +0700

    rename  register .html to .php, add connect.php

---
+++ PHP/connect.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "admin";
+$password = "admin";
+$database = "tabunganku";
+
+// create a connection
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// check the connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connection successful";
+?>
