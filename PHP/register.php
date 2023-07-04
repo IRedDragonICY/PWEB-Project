b044259f672986754e0bf4f162ab36f60fe46d41
Ndik <hendik.suwoto@gmail.com>
2023-07-04 11:00:13 +0700

    remove comment

--- PHP/register.php
+++ PHP/register.php
@@ -4,20 +4,14 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-//membuat query untuk insert data ke database
 $sql = "INSERT INTO accountdb (name, email, password, role)
 VALUES ('$name', '$email', '$password', 0)";
 
-// membuat database untuk user sesuai dengan username
-
-
-//mengeksekusi query dan mengecek apakah berhasil atau tidak
 if ($conn->query($sql) === TRUE) {
   echo "User berhasil didaftarkan";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// redirect ke dashboard
 header("Location: ../dashboard.html");
 ?>
